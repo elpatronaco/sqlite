5881ada7d84373129a32bf3caf334d70cc286272
Pau <paucolome10@gmail.com>
2020-12-25 20:09:41 +0100

    Init 🔥

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:sqliteapp/ui/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Reactive Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        canvasColor: Colors.transparent
+      ),
+      //Our only screen/page we have
+      home: HomePage(title: 'My Todo List'),
+    );
+  }
+}
+
